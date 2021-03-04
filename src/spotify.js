5ce903e6182ae1945de02a38469a063a57d5dfd2
Abhijeet Kumar <44672451+Abhijeet05061998@users.noreply.github.com>
2021-03-04 23:30:40 +0530

    Update spotify.js

--- src/spotify.js
+++ src/spotify.js
@@ -1,6 +1,6 @@
-// https://developer.spotify.com/documentation/web-playback-sdk/quick-start/#
+
 export const authEndpoint = "https://accounts.spotify.com/authorize";
-// Replace with your app's client ID, redirect URI and desired scopes
+
 const clientId = "cff76213089a4e228206c8bf120d67cb";
 const redirectUri = "http://localhost:3000/";
 const scopes = [
@@ -22,7 +22,7 @@ export const getTokenFromResponse = () => {
       return initial;
     }, {});
 };
-
+//Acess url
 export const accessUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
   "%20"
 )}&response_type=token&show_dialog=true`;
